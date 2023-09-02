585d0fdf679fe81ffbbfc889fd1d7bfbe03b32a4
José C B Junior <142643035+ZehHey@users.noreply.github.com>
2023-09-01 22:03:07 -0300

    Aula 03 pg02

--- Aula03/teste.js
+++ Aula03/teste.js
@@ -1,7 +1,29 @@
-var pais = 'Brasil'
-console.log(`Vivendo em ${pais}`)
-if (pais == 'Brasil') {
-    console.log('Você é brasileiro.')
-} else {
-    console.log('Você é estrangeiro.')
+var agora = new Date()
+var diaSem = agora.getDay()
+
+switch(diaSem){
+    case 0: 
+        console.log('Domingo')
+        break
+    case 1: 
+        console.log('Segunda-Feira')
+        break
+    case 2:
+        console.log('Terça-Feira')
+        break
+    case 3:
+        console.log('Quarta-Feira')
+        break
+    case 4:
+        console.log('Quinta-Feira')
+        break
+    case 5: 
+        console.log('Sexta-Feira')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Erro')
+        break
 }
