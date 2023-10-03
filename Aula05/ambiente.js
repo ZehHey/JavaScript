3e1e4d9d0ace6431ad14f8eb3941bd9f8fdd026a
José C B Junior <jcbuchner.jr@gmail.com>
2023-10-03 19:24:55 -0300

    aula 05 funçoes

--- Aula05/ambiente.js
+++ Aula05/ambiente.js
@@ -1,6 +1,10 @@
-let num = [8, 6, 7, 2, 4]
-num.sort()
-console.log(num)
-for(let pos in num) {
-    console.log(`Na posição ${pos} temos o valor ${num[pos]}`)
+function parimp (n) {
+    if (n%2==0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
 }
+
+let res = parimp(9)
+console.log(res)
